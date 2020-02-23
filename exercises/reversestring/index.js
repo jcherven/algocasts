cc53fff4767b16cddc0bc94b675d6a41e153cd8a
jcherven <cherven.james@gmail.com>
2020-02-23 15:21:41 -0500

    Adds three ways to solve 'reverse'

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // using reverse array helper
+  // return str
+  //   .split('')
+  //   .reverse()
+  //   .join('');
+  // using es6 terse for loop
+  // let reversed = '';
+  // for (let char of str) {
+  //   reversed = char + reversed;
+  // }
+  // return reversed;
+  // using es6 reduce()
+  // return str.split('').reduce((reversed, char) => char + reversed, '');
+}
 
 module.exports = reverse;
