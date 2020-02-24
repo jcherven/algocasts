2059f69164b34a658825c5fb03095cad2cc29485
jcherven <cherven.james@gmail.com>
2020-02-24 15:22:14 -0500

    adds a solution to fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,18 +13,32 @@
 //   buzz
 
 function fizzBuzz(n) {
-  //*** straightforward way is generally the best way for fizzbuzz
-  // for (let i = 1; i <= n; i++) {
-  //   if (i % 3 === 0 && i % 5 === 0) {
-  //     console.log('fizzbuzz');
-  //   } else if (i % 5 === 0) {
-  //     console.log('buzz');
-  //   } else if (i % 3 === 0) {
-  //     console.log('fizz');
-  //   } else {
-  //     console.log(i);
-  //   }
-  // }
+  for (let i = 1; i <= n; i++) {
+    //*** straightforward way is generally the best way for fizzbuzz
+    //   if (i % 3 === 0 && i % 5 === 0) {
+    //     console.log('fizzbuzz');
+    //   } else if (i % 5 === 0) {
+    //     console.log('buzz');
+    //   } else if (i % 3 === 0) {
+    //     console.log('fizz');
+    //   } else {
+    //     console.log(i);
+    //   }
+    //*** using a switch/case statement
+    switch (true) {
+      case i % 3 === 0 && i % 5 === 0:
+        console.log('fizzbuzz');
+        break;
+      case i % 3 === 0:
+        console.log('fizz');
+        break;
+      case i % 5 === 0:
+        console.log('buzz');
+        break;
+      default:
+        console.log(i);
+    }
+  }
 }
 
 module.exports = fizzBuzz;
