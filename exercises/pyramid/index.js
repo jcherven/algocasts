fdc5d2593354e439cda4b16d81dac3eeaac7bd12
jcherven <cherven.james@gmail.com>
2020-02-26 16:40:13 -0500

    adds solutions for pyramid and vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,49 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+//*** Straighforward iterative tactic
+/* 0. Determine a row's midpoint for n columns ( Math.floor(2 * n - 1) / 2 )
+ * 1. For 0 to n (iterate through rows)
+ *   1. Create an empty string level
+ *   2. For 0 to (2n - 1) columns
+ *     1. If midpoint - row <= column AND midpoint + row >= column
+ *       1. Add a # to level
+ *     2. Else
+ *       1. Add a space to level
+ *   3. Console.log stair
+ */
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+//*** Recursive tactic
+// function pyramid(n, row = 0, level = '') {
+//   if (row === n) {
+//     return;
+//   }
+//   if (level.length === 2 * n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   let add;
+//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//     add = '#';
+//   } else {
+//     add = ' ';
+//   }
+//   pyramid(n, row, level + add);
+// }
 
 module.exports = pyramid;
