55eb6ee278d5cbf75e0d85c8904119d170eb435d
jcherven <cherven.james@gmail.com>
2020-03-01 21:31:06 -0500

    adds for...of functionality to the linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -28,20 +28,22 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head;
+    return this.getAt(0);
+    // return this.head;
   }
 
   getLast() {
-    if (!this.head) {
-      return null;
-    }
-    let node = this.head;
-    while (node) {
-      if (!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
+    return this.getAt(this.size() - 1);
+    // if (!this.head) {
+    //   return null;
+    // }
+    // let node = this.head;
+    // while (node) {
+    //   if (!node.next) {
+    //     return node;
+    //   }
+    //   node = node.next;
+    // }
   }
 
   clear() {
@@ -49,27 +51,29 @@ class LinkedList {
   }
 
   removeFirst() {
-    if (!this.head) {
-      return;
-    }
-    this.head = this.head.next;
+    return this.removeAt(0);
+    // if (!this.head) {
+    //   return;
+    // }
+    // this.head = this.head.next;
   }
 
   removeLast() {
-    if (!this.head) {
-      return;
-    }
-    if (!this.head.next) {
-      this.head = null;
-      return;
-    }
-    let previous = this.head;
-    let node = this.head.next;
-    while (node.next) {
-      previous = node;
-      node = node.next;
-    }
-    previous.next = null;
+    return this.removeAt(this.size() - 1);
+    // if (!this.head) {
+    //   return;
+    // }
+    // if (!this.head.next) {
+    //   this.head = null;
+    //   return;
+    // }
+    // let previous = this.head;
+    // let node = this.head.next;
+    // while (node.next) {
+    //   previous = node;
+    //   node = node.next;
+    // }
+    // previous.next = null;
   }
 
   insertLast(data) {
@@ -121,6 +125,24 @@ class LinkedList {
     const previous = this.getAt(index - 1) || this.getLast();
     const node = new Node(data, previous.next);
     previous.next = node;
+  }
+
+  forEach() {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
   }
 }
 module.exports = { Node, LinkedList };
